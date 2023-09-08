90145290387629aabcf6b3a9107f079708c8bfcc
Willeujeans <Willeujeans@gmail.com>
2023-09-07 20:37:38 -0600

    added notifications

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 let scoreElm = document.getElementById("count");
 
+
 function setScore(num){
     chrome.storage.sync.set({"score": num }).then(() => {
         updateVisualScore(num);
+        checkForGoal(num);
     });
 }
 
@@ -26,6 +28,23 @@ function storageCheck(){
         }
         updateVisualScore();
     });
+}
+
+function checkForGoal(num){
+    if(num == 10){
+        var message = "Thats a whole " + num + " clicks!";
+        sendMessage("NICE!", message);
+    }
+}
+
+function sendMessage(title, message){
+    var notifOptions = {
+        type:"basic",
+        iconUrl: "icon48.png",
+        title: title,
+        message: message
+    };
+    chrome.notifications.create("messageNotif", notifOptions);
 }
 
 document.body.addEventListener("mousedown",(event)=>{
